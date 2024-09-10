b68f741816257f81255f9b8e8f0e536997c94c0a
Johnmclf <150351096+Johnmclf@users.noreply.github.com>
2024-09-10 16:52:19 +0200

    Traitez l'envoi en PHP

--- submit_contact.php
+++ submit_contact.php
@@ -25,6 +25,28 @@ return;
 <body class="d-flex flex-column min-vh-100">
     <div class="container">
 
+        <?php
+    // Testons si le fichier a bien été envoyé et s'il n'y a pas d'erreur
+    if (isset($_FILES['screenshot']) && $_FILES['screenshot']['error'] == 0)
+    {
+        // Testons si le fichier n'est pas trop gros
+        if ($_FILES['screenshot']['size'] <= 1000000)
+        {
+            // Testons si l'extension est autorisée
+            $fileInfo = pathinfo($_FILES['screenshot']['name']);
+            $extension = $fileInfo['extension'];
+            $allowedExtensions = ['jpg', 'jpeg', 'gif', 'png'];
+            if (in_array($extension, $allowedExtensions))
+            {
+                // On peut valider le fichier et le stocker définitivement
+                move_uploaded_file($_FILES['screenshot']['tmp_name'],
+                'uploads/' . basename($_FILES['screenshot']['name']));
+                echo "L'envoi a bien été effectué !";
+            }
+        }
+    }
+    ?>
+
     <?php include_once('header.php'); ?>
         <h1>Site de recettes</h1>
 
